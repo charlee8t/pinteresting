4f1c46cd21e9ad8293341660ecfeb1295efeba47
Carlos Yusim <charlee8t@yahoo.com>
2014-08-20 06:58:41 -0400

    changed root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :flows
 
   devise_for :users
-root "pages#home"
+root "flows#index"
 get "about" => "pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
